b5197738fcc31dc0cc2d7a11b3272a3beab0f51d
Siddharth Singh Gour <siddharthsinghgour@Siddharths-MacBook-Air.local>
2023-01-13 20:19:09 +0530

    Api Get Request Axios Storing In Redux

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,10 +1,16 @@
 import { GET_USER } from "./types";
+import axios from "axios";
 
-const getUser = (text) => (dispatch) => {
-  dispatch({
-    type: GET_USER,
-    payload: "User_One",
-  });
+const getUser = () => async (dispatch) => {
+  try {
+    const res = await axios.get("https://reqres.in/api/users?page=2");
+    dispatch({
+      type: GET_USER,
+      payload: res.data.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getUser };
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -5,7 +5,7 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case GET_USER:
-      return [...state, payload];
+      return [...state, ...payload];
     default:
       return state;
   }
